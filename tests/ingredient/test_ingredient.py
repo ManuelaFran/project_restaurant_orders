c13965e67b3aaa1e13c86044befbcc6dbd06a09e
Manuela França <manuelaalves26@gmail.com>
2023-05-16 10:00:16 -0300

    test: implementado teste da classe Ingredient.

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,5 +3,11 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    pass
-# iniciando o projeto
+    cheese_ingredient = Ingredient("queijo mussarela")
+    sugar_ingredient = Ingredient("sugar")
+
+    assert sugar_ingredient.__hash__() == hash(sugar_ingredient.name)
+    assert sugar_ingredient.__eq__(sugar_ingredient) == True  # noqa E712
+    assert sugar_ingredient.__eq__(cheese_ingredient) == False  # noqa E712
+    assert sugar_ingredient.__repr__() == "Ingredient('sugar')"
+    assert len(cheese_ingredient.restrictions) == 2
