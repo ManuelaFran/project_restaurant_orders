67e1f5116cb9affb6566eace6e34aa1d0c4e9ce5
Manuela França <manuelaalves26@gmail.com>
2023-05-16 12:41:28 -0300

    feat: implementada a classe MenuData que faz o mapeamento de pratos e ingredientes baseado no arquivo csv.

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,33 @@
+import pandas as pd
+from models.dish import Dish
+from models.ingredient import Ingredient
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+        self.open_csv_file(source_path)
+
+    def open_csv_file(self, source_path):
+        data = pd.read_csv(source_path)
+        dishes_list = []
+        current_dish = ""
+
+        for _, dish_data in data.iterrows():
+            if current_dish != dish_data["dish"]:
+                current_dish = dish_data["dish"]
+                new_dish = Dish(dish_data["dish"], float(dish_data["price"]))
+                new_dish.add_ingredient_dependency(
+                    Ingredient(dish_data["ingredient"]),
+                    int(dish_data["recipe_amount"]),
+                )
+                dishes_list.append(new_dish)
+            if current_dish == dish_data["dish"]:
+                dishes_list[-1].add_ingredient_dependency(
+                    Ingredient(dish_data["ingredient"]),
+                    int(dish_data["recipe_amount"]),
+                )
+
+        for dish_dependency in dishes_list:
+            self.dishes.add(dish_dependency)
